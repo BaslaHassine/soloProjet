d0cda21492ef1c96e4da6992472f3f0164b6857b
BaslaHassine <turkibaslahassine@gmail.com>
2021-01-11 13:39:33 +0100

    h

--- CaLcU.js
+++ CaLcU.js
@@ -1,1 +1,22 @@
+window.onload= function(){
 
+$('#add').click(function(){
+	var name = $('#idcommodityNameAdd').val();
+	var qte  = $('#Qte').val();
+	var price= $('#idprice').val();
+	console.log(price)
+	var total= parseFloat($('#total').val()) + (parseFloat(qte, 100) * parseFloat(price, 100));
+	$("#total").val(total);
+	var amount = $("#idamount").val();
+	var remainder = parseFloat(amount, 100) - parseFloat(total, 100);
+	$("#idremainder").val(remainder);
+
+});
+
+
+
+
+
+
+
+}
